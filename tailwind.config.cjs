4479da8ac84d369e635d350f2cde02011c466ef4
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-05-16 19:15:33 +0200

    Add/customize themes (no switch function, tho)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,36 @@
 const daisyui = require("daisyui");
 const typography = require("@tailwindcss/typography");
 
+const wavebreakerBrandColor = "#009EFF";
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-
   theme: {
     fontFamily: {
       'sans': ['Inter', 'system-ui', 'sans-serif'],
     },
     extend: {}
   },
-
+  daisyui: {
+    themes: [{
+      dark: {
+        ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+        primary: wavebreakerBrandColor,
+        neutral: "#212337",
+        "base-100": "#1B1E2B",
+        "base-content": "#c8d3f5"
+      },
+    },
+    {
+      light: {
+        ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+        primary: wavebreakerBrandColor,
+        "base-content": "#000000"
+      },
+    },
+    ],
+  },
   plugins: [typography, daisyui]
 };
 
