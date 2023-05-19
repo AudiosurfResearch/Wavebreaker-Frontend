f384f5544116954d6c7c022b988fbfeee24310e6
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-05-20 00:27:34 +0200

    Fiddle with the user page

--- src/routes/user/[id=integer]/+page.svelte
+++ src/routes/user/[id=integer]/+page.svelte
@@ -10,22 +10,37 @@
 	<title>Wavebreaker | {data.userResult.username}</title>
 </svelte:head>
 
-<div class="hero">
-	<div class="hero-content rounded-3xl bg-neutral flex-col lg:flex-row">
-		<div class="avatar">
-			<div class="max-w-xs lg:max-w-sm rounded-3xl mr-4">
-				<img src={data.userResult.avatarUrl} alt="Avatar of {data.userResult.username}" />
+<div
+	class="flex p-4 gap-4 rounded-3xl bg-neutral w-full justify-start items-center flex-col lg:flex-row"
+>
+	<div class="avatar self-start">
+		<div class="max-w-xs lg:max-w-sm rounded-3xl mr-4">
+			<img src={data.userResult.avatarUrl} alt="Avatar of {data.userResult.username}" />
+		</div>
+	</div>
+	<div class="flex flex-col self-start lg:self-center gap-y-2">
+		<div class="flex flex-row items-center flex-wrap">
+			<div class="max-w-5xl mr-3">
+				<h1 class="text-3xl lg:text-5xl font-bold break-words">
+                    <!-- TODO: Try to either wrap or cut off extremely long usernames somehow -->
+					{data.userResult.username}
+				</h1>
 			</div>
+			{#if data.userResult.accountType == 3}
+				<UserBadge
+					classExt="bg-gradient-to-r from-teal-400 to-primary my-1"
+					imgSrc={teamBadgeIcon}
+					label={'Wavebreaker Team'}
+				/>
+			{:else if data.userResult.accountType == 2}
+				<UserBadge classExt="bg-error" label={'Moderator'} />
+			{/if}
 		</div>
-		<div class="max-w-5xl">
-			<h1 class="flex items-center text-5xl font-bold">
-				{data.userResult.username}
-			</h1>
+		<div class="ml-1">
+			<b>Total score:</b> 69696969<br />
+			<b>Favorite character:</b> Ninja Mono<br />
+            put some more stuff here, idk what<br />
+            these are merely placeholders
 		</div>
-		{#if data.userResult.accountType == 3}
-			<UserBadge classExt="bg-gradient-to-r from-teal-400 to-primary" imgSrc={teamBadgeIcon} label={"Wavebreaker Team"}/>
-		{:else if data.userResult.accountType == 2}
-			<UserBadge classExt="bg-error" label={"Moderator"}/>
-		{/if}
 	</div>
 </div>
