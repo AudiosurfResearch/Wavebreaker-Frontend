c6ae83e719c1e4a672633ccf9132bca0d6b736a4
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-06-17 17:38:37 +0200

    Try adding Open Graph stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { env } from '$env/dynamic/public';
 	import logo from '$lib/assets/wavebreaker_icon.svg';
 </script>
 
@@ -8,6 +9,13 @@
 		name="description"
 		content="Home page of Wavebreaker, an open-source Audiosurf 1 server reimplementation."
 	/>
+	<meta content="Wavebreaker" property="og:title" />
+	<meta
+		content="An open-source reimplementation of Audiosurf's server. View your scores, songs and rivals!"
+		property="og:description"
+	/>
+	<meta content={env.PUBLIC_FRONTEND_URL} property="og:url" />
+	<meta content="#009EFF" name="theme-color" />
 </svelte:head>
 
 <div class="flex flex-col items-center lg:flex-row-reverse px-7 max-w-full">
