4b2305c8a0fecf26cad23d6f37dcf3b667d24a6f
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-06-27 16:10:39 +0200

    Better message when logging in while unregistered

--- src/routes/login/steamreturn/+page.server.ts
+++ src/routes/login/steamreturn/+page.server.ts
@@ -1,26 +1,33 @@
 import { dev } from '$app/environment';
 import { fetcher } from '$lib/api';
 import type { UserInfo, UserTokenResponse } from '$lib/models/UserData';
-import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
+import { error, redirect, type ServerLoadEvent } from '@sveltejs/kit';
+import { isAxiosError } from 'axios';
 
 //Getting this to work took me at least 4 hours. This is a cry for help.
 //Nevermind. Make that at least 6.
 //still not working.
 //nvm finally.
 export const load = async ({ url, cookies, locals }: ServerLoadEvent) => {
-	const tokenResponse = await fetcher<UserTokenResponse>(
-		'/api/auth/steam/return?' + url.searchParams.toString()
-	);
-	
-	cookies.set('Authorization', tokenResponse.token, {
-		path: '/',
-		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 28), // 4 weeks
-		secure: !dev,
-	});
+	try {
+		const tokenResponse = await fetcher<UserTokenResponse>(
+			'/api/auth/steam/return?' + url.searchParams.toString()
+		);
 
-	const user = await fetcher<UserInfo>('/api/auth/verifyToken', tokenResponse.token);
-	locals.user = user;
-	console.log("Logged in as user " + user.id)
+		cookies.set('Authorization', tokenResponse.token, {
+			path: '/',
+			expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 28), // 4 weeks
+			secure: !dev
+		});
 
-	throw redirect(307, '/');
+		const user = await fetcher<UserInfo>('/api/auth/verifyToken', tokenResponse.token);
+		locals.user = user;
+		console.log('Logged in as user ' + user.id);
+
+		throw redirect(307, '/');
+	} catch (e) {
+		if (isAxiosError(e) && e.response)
+			return error(e.response?.status, { message: e.response.data.error });
+		else throw e;
+	}
 };
