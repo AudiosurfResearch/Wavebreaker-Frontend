41970d6921ac9e02d1a2dae9bb1c3ca6282fea54
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-07-17 19:36:52 +0200

    Add more error page song quotes

--- src/lib/songQuoteList.ts
+++ src/lib/songQuoteList.ts
@@ -6,42 +6,66 @@ interface SongQuote {
 export const songQuoteList: SongQuote[] = [
 	{
 		content: "Good evening ladies and gentlemen, what you're about to get here is real",
-		link: "https://youtu.be/Ie-3zpOaw6w"
+		link: 'https://youtu.be/Ie-3zpOaw6w'
 	},
 	{
 		content: "The rainy wanderers're cursed with true love",
-		link: "https://youtu.be/0fu06PW81x0"
+		link: 'https://youtu.be/0fu06PW81x0'
 	},
-    {
-		content: "I know, I know, I know you were not here",
-		link: "https://youtu.be/yVDB7sRPLiA"
+	{
+		content: 'I know, I know, I know you were not here',
+		link: 'https://youtu.be/yVDB7sRPLiA'
 	},
-    {
-		content: "I see myself, dancing in the puddles", //shoutout to asuzora
-		link: "https://bycolourcrew.bandcamp.com/track/live-once-2"
+	{
+		content: 'I see myself, dancing in the puddles', //shoutout to asuzora
+		link: 'https://bycolourcrew.bandcamp.com/track/live-once-2'
 	},
 	{
 		content: "You'll see it's about to rain",
-		link: "https://soundcloud.com/lapixmusic/spd-garcrossfade#t=12%3A04"
+		link: 'https://soundcloud.com/lapixmusic/spd-garcrossfade#t=12%3A04'
 	},
 	{
 		content: "All I know is, it's just for the summer",
-		link: "https://youtu.be/sBRjaQIub_g"
+		link: 'https://youtu.be/sBRjaQIub_g'
 	},
 	{
-		content: "What a silly tale!",
-		link: "https://youtu.be/1hvTktgYORo"
+		content: 'What a silly tale!',
+		link: 'https://youtu.be/1hvTktgYORo'
 	},
 	{
 		content: "It's crystal clear to me",
-		link: "https://youtu.be/HgMGW3vjLVk"
+		link: 'https://youtu.be/HgMGW3vjLVk'
 	},
 	{
-		content: "Jack the broadcasting room!",
-		link: "https://youtu.be/lo603GRjNvI"
+		content: 'Jack the broadcasting room!',
+		link: 'https://youtu.be/lo603GRjNvI'
 	},
 	{
-		content: "Moon shines in the cranberry city, our stories taste sour but sweet",
-		link: "https://youtu.be/TLpgGajZgHE"
+		content: 'Moon shines in the cranberry city, our stories taste sour but sweet',
+		link: 'https://youtu.be/TLpgGajZgHE'
 	},
+	{
+		content: 'Which one is me?',
+		link: 'https://soundcloud.com/poplavor/i-am-feat-kanatan'
+	},
+	{
+		content: "Let's do what you want",
+		link: 'https://youtu.be/Y4Y3zKDoUZo'
+	},
+	{
+		content: 'Tune up together',
+		link: 'https://soundcloud.com/dada-qada/galaxy-vacation-feat'
+	},
+	{
+		content: 'Cups to the sky, if you feel the vibe',
+		link: 'https://youtu.be/_Xr5ZQnOSEA'
+	},
+	{
+		content: 'Unlimited sleep deprivation',
+		link: 'https://youtu.be/QsgJNYzlA9U'
+	},
+	{
+		content: 'Life is funny',
+		link: 'https://youtu.be/CXQN1eVmzMA'
+	}
 ];
