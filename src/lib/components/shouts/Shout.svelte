7e62a41e5a76991c86238141760a5a26d0670c2d
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-07-29 22:42:42 +0200

    maybe dont do that

--- src/lib/components/shouts/Shout.svelte
+++ src/lib/components/shouts/Shout.svelte
@@ -23,7 +23,7 @@
 			<p class="text-base-content/60">{format(shout.timeCreated)}</p>
 
 			{#if currentUser && (currentUser.id == shout.authorId || currentUser.accountType >= 2)}
-				<form method="POST" action="?/deleteShout" use:enhance>
+				<form method="POST" action="?/deleteShout">
 					<input type="hidden" name="shoutId" value={shout.id} />
 					<button class="btn btn-square btn-xs btn-ghost text-error"><Fa icon={faTrash} /></button>
 				</form>
