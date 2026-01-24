a2b8399131068a4fdbb77ecf52e5f5e5e63d0d3a
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2026-01-25 00:14:55 +0100

    navbar stuff, robots.txt, favicon

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,22 +3,38 @@
 
     import { Button } from "$lib/components/ui/button/index.js";
     import { Badge } from "$lib/components/ui/badge/index.js";
+    import wavebreakerIcon from "$lib/assets/wavebreaker-icon.svg";
+    import DarkModeToggle from "./DarkModeToggle.svelte";
 </script>
 
 <header class="bg-background sticky top-0 z-50 w-full">
-    <div class="container-wrapper 3xl:fixed:px-0 px-6">
-        <div class="3xl:fixed:container flex h-16 items-center gap-2">
-            <Button href="/" variant="ghost" class="lg:flex">
-                who up waving they breaker
-            </Button>
-            {#if dev}
-                <Badge
-                    class="h-5 min-w-5 rounded-full text-warning-foreground bg-warning"
-                    variant="outline"
-                >
-                    development
-                </Badge>
-            {/if}
+    <div class="container-wrapper">
+        <div class="flex h-16 items-center justify-between mx-auto px-2">
+            <div class="order-1 inline-flex items-center justify-start w-1/2">
+                <Button href="/" variant="ghost" class="flex gap-4 h-12">
+                    <img
+                        alt="Wavebreaker logo"
+                        class="w-8"
+                        src={wavebreakerIcon}
+                    />
+                    <div class="hidden sm:block text-2xl font-semibold">
+                        Wavebreaker
+                    </div>
+                </Button>
+                {#if dev}
+                    <Badge class="text-warning-foreground bg-warning">
+                        development
+                    </Badge>
+                {/if}
+            </div>
+            <div class="order-1 hidden md:inline-flex items-center shrink-0">
+                <div>placeholder</div>
+            </div>
+            <div
+                class="order-2 inline-flex items-center mr-4 justify-end w-1/2"
+            >
+                <DarkModeToggle />
+            </div>
         </div>
     </div>
 </header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "./layout.css";
-    import favicon from "$lib/assets/favicon.svg";
+    import faviconPng from "$lib/assets/favicon.png";
+    import faviconSvg from "$lib/assets/wavebreaker-icon.svg";
     import { ModeWatcher } from "mode-watcher";
     import * as Tooltip from "$lib/components/ui/tooltip/index.js";
     import Navbar from "$lib/components/Navbar.svelte";
@@ -9,7 +10,10 @@
 </script>
 
 <ModeWatcher />
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <link rel="icon" href={faviconSvg} type="image/svg+xml" />
+    <link rel="icon" href={faviconPng} type="image/png" />
+</svelte:head>
 <div class="bg-background relative flex min-h-svh flex-col">
     <Navbar />
     <main class="flex flex-1 flex-col">
