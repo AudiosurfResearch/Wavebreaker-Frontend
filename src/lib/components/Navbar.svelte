c78033038bef3b5d5cdae7d859caa203740ccc34
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2026-01-25 12:21:48 +0100

    feat: Add middle buttons to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
     import { dev } from "$app/environment";
 
+    import HomeIcon from "@lucide/svelte/icons/home";
+    import UserIcon from "@lucide/svelte/icons/user";
+    import MusicIcon from "@lucide/svelte/icons/music";
+    import TrophyIcon from "@lucide/svelte/icons/trophy";
+
     import { Button } from "$lib/components/ui/button/index.js";
     import { Badge } from "$lib/components/ui/badge/index.js";
+    import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
     import wavebreakerIcon from "$lib/assets/wavebreaker-icon.svg";
-    import DarkModeToggle from "./DarkModeToggle.svelte";
+    import DarkModeToggle from "$lib/components/DarkModeToggle.svelte";
+
+    import { page } from "$app/state";
+
+    let isRoot = $derived(page.url.pathname == "/");
+    let isUserPage = $derived(page.url.pathname.startsWith("/users"));
+    let isSongPage = $derived(page.url.pathname.startsWith("/songs"));
+    let isRankingPage = $derived(page.url.pathname.startsWith("/rankings"));
 </script>
 
 <header class="bg-background sticky top-0 z-50 w-full">
@@ -20,15 +33,48 @@
                     <div class="hidden sm:block text-2xl font-semibold">
                         Wavebreaker
                     </div>
+                    {#if dev}
+                        <Badge class="text-warning-foreground bg-warning">
+                            development
+                        </Badge>
+                    {/if}
                 </Button>
-                {#if dev}
-                    <Badge class="text-warning-foreground bg-warning">
-                        development
-                    </Badge>
-                {/if}
             </div>
             <div class="order-1 hidden md:inline-flex items-center shrink-0">
-                <div>placeholder</div>
+                <ButtonGroup.Root>
+                    <Button
+                        href="/"
+                        variant="ghost"
+                        size="sm"
+                        class={{ "text-primary": isRoot }}
+                    >
+                        <HomeIcon /> Home
+                    </Button>
+                    <Button
+                        href="/users/search"
+                        variant="ghost"
+                        size="sm"
+                        class={{ "text-primary": isUserPage }}
+                    >
+                        <UserIcon /> Users
+                    </Button>
+                    <Button
+                        href="/songs/search"
+                        variant="ghost"
+                        size="sm"
+                        class={{ "text-primary": isSongPage }}
+                    >
+                        <MusicIcon /> Songs
+                    </Button>
+                    <Button
+                        href="/rankings"
+                        variant="ghost"
+                        size="sm"
+                        class={{ "text-primary": isRankingPage }}
+                    >
+                        <TrophyIcon /> Rankings
+                    </Button>
+                </ButtonGroup.Root>
             </div>
             <div
                 class="order-2 inline-flex items-center mr-4 justify-end w-1/2"
